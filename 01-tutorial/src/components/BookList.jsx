5c3ced6319318c5e9c6ea54643b9397355cf8137
vsbron <vsbron@gmail.com>
2025-07-01 14:27:33 +0300

    Export books data into a separate file with export

--- 01-tutorial/src/components/BookList.jsx
+++ 01-tutorial/src/components/BookList.jsx
@@ -1,48 +1,8 @@
 import Book from "./Book";
 
-function BookList() {
-  // Initializing the books details
-  const books = [
-    {
-      cover: "book-1.jpg",
-      title: "The Lost Bookshop",
-      subtitle:
-        "The most charming and uplifting novel for 2025 and the perfect gift for book lovers!",
-      author: "Evie Woods",
-      id: "1",
-    },
-    {
-      cover: "book-2.jpg",
-      title: "The Housemaid",
-      author: "Freida McFadden",
-      id: "2",
-    },
-    {
-      cover: "book-3.jpg",
-      title: "Every Summer After",
-      author: "Carley Fortune",
-      id: "3",
-    },
-    {
-      cover: "book-4.jpg",
-      title: "One Golden Summer",
-      author: "Carley Fortune",
-      id: "4",
-    },
-    {
-      cover: "book-5.jpg",
-      title: "The First Gentleman: A Thriller",
-      author: "James Patterson & Bill Clinton",
-      id: "5",
-    },
-    {
-      cover: "book-6.jpg",
-      title: "The Wedding People: A Novel",
-      author: "Alison Espach",
-      id: "6",
-    },
-  ];
+import { books } from "../lib/books";
 
+function BookList() {
   // Function that gets the book info based on the passed id
   const getBook = (id) => {
     const chosenBook = books.find((book) => book.id === id);
---
+++ 01-tutorial/src/lib/books.js
@@ -0,0 +1,40 @@
+export const books = [
+  {
+    cover: "book-1.jpg",
+    title: "The Lost Bookshop",
+    subtitle:
+      "The most charming and uplifting novel for 2025 and the perfect gift for book lovers!",
+    author: "Evie Woods",
+    id: "1",
+  },
+  {
+    cover: "book-2.jpg",
+    title: "The Housemaid",
+    author: "Freida McFadden",
+    id: "2",
+  },
+  {
+    cover: "book-3.jpg",
+    title: "Every Summer After",
+    author: "Carley Fortune",
+    id: "3",
+  },
+  {
+    cover: "book-4.jpg",
+    title: "One Golden Summer",
+    author: "Carley Fortune",
+    id: "4",
+  },
+  {
+    cover: "book-5.jpg",
+    title: "The First Gentleman: A Thriller",
+    author: "James Patterson & Bill Clinton",
+    id: "5",
+  },
+  {
+    cover: "book-6.jpg",
+    title: "The Wedding People: A Novel",
+    author: "Alison Espach",
+    id: "6",
+  },
+];
