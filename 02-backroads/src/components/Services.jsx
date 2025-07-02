d93cd889883b73a5764b3187207372ca29c04593
vsbron <vsbron@gmail.com>
2025-07-02 13:47:11 +0300

    02-backroads - Add dynamic content to Services component

--- 02-backroads/src/components/Services.jsx
+++ 02-backroads/src/components/Services.jsx
@@ -1,3 +1,4 @@
+import { services } from "../lib/data";
 import Title from "./ui/Title";
 
 function Services() {
@@ -6,44 +7,17 @@ function Services() {
     <section className="section services" id="services">
       <Title title="Our" subtitle="services" />
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-wallet fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-tree fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-socks fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
+        {services.map((service) => (
+          <article className="service">
+            <span className="service-icon">
+              <i className={service.icon}></i>
+            </span>
+            <div className="service-info">
+              <h4 className="service-title">{service.title}</h4>
+              <p className="service-text">{service.text}</p>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
