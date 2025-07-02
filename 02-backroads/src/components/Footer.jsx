2db98b5c389e281b5da70da03fe7e662b874f0ba
vsbron <vsbron@gmail.com>
2025-07-02 14:18:38 +0300

    02-backroads - Add dynamic content to Footer component

--- 02-backroads/src/components/Footer.jsx
+++ 02-backroads/src/components/Footer.jsx
@@ -1,60 +1,31 @@
+import { pageLinks, socialLinks } from "../lib/data";
+
 function Footer() {
   // Returned JSX
   return (
     <footer className="section footer">
       <ul className="footer-links">
-        <li>
-          <a href="#home" className="footer-link">
-            home
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="footer-link">
-            about
-          </a>
-        </li>
-        <li>
-          <a href="#services" className="footer-link">
-            services
-          </a>
-        </li>
-        <li>
-          <a href="#featured" className="footer-link">
-            featured
-          </a>
-        </li>
+        {pageLinks.map((link) => (
+          <li key={link.id}>
+            <a href={link.href} className="footer-link">
+              {link.text}
+            </a>
+          </li>
+        ))}
       </ul>
       <ul className="footer-icons">
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            rel="noreferrer"
-            className="footer-icon"
-          >
-            <i className="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            rel="noreferrer"
-            className="footer-icon"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            rel="noreferrer"
-            className="footer-icon"
-          >
-            <i className="fab fa-squarespace"></i>
-          </a>
-        </li>
+        {socialLinks.map((link) => (
+          <li key={link.id}>
+            <a
+              href={link.href}
+              target="_blank"
+              rel="noreferrer"
+              className="footer-icon"
+            >
+              <i className={link.icon}></i>
+            </a>
+          </li>
+        ))}
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
