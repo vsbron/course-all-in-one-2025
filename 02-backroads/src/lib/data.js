dbb0de9075535664726220a74ce4fa088da9fab9
vsbron <vsbron@gmail.com>
2025-07-02 12:39:03 +0300

    02-backroads - Set the header component. Move links to a data.js file

---
+++ 02-backroads/src/lib/data.js
@@ -0,0 +1,12 @@
+export const pageLinks = [
+  { id: 1, href: "#home", text: "Home" },
+  { id: 2, href: "#about", text: "About" },
+  { id: 3, href: "#services", text: "Services" },
+  { id: 4, href: "#tours", text: "Tours" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://twitter.com/", icon: "fab fa-twitter" },
+  { id: 2, href: "https://facebook.com/", icon: "fab fa-facebook" },
+  { id: 3, href: "https://instagram.com", icon: "fab fa-instagram" },
+];
