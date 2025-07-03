8df31279e5e63e5b3abc5f241231dfe1d2a8fd06
vsbron <vsbron@gmail.com>
2025-07-03 14:07:17 +0300

    03-advanced-react - Add a list component based on a state with the options to remove item and clear the list

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,11 @@
-import UseStateBasics from "./tutorial/01-useState/02-useState-basics";
+import UseStateArray from "./tutorial/01-useState/03-useState-array";
 
 function App() {
   // Returned JSX
   return (
     <div className="container">
       <h2>Advanced React</h2>
-      <UseStateBasics />
+      <UseStateArray />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/01-useState/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/03-useState-array.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+
+import { people } from "../../data";
+
+// Setting the initial people data
+const initialData = people;
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  // Setting the state of people
+  const [people, setPeople] = useState(initialData);
+
+  // Setting click handlers
+  const handleRemove = (id) => {
+    setPeople((ppl) => ppl.filter((p) => p.id !== id));
+  };
+  const handleClear = () => {
+    setPeople([]);
+  };
+
+  // Returned JSX
+  return (
+    <>
+      <h2>useState array example</h2>
+      <ul>
+        {people.map((ppl) => (
+          <li key={ppl.id}>
+            {ppl.name}{" "}
+            <button onClick={() => handleRemove(ppl.id)} className="btn">
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClear} className="btn">
+        Clear the list
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
