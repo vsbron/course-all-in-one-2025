520b1ddee15ec8fa04671a1f417309af449cf05d
vsbron <vsbron@gmail.com>
2025-07-03 13:06:17 +0300

    03-advanced-react - Set up wrong example of counter increase, without the state

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,8 +1,11 @@
+import ErrorExample from "./tutorial/01-useState/01-error-example";
+
 function App() {
+  // Returned JSX
   return (
     <div className="container">
       <h2>Advanced React</h2>
-      
+      <ErrorExample />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/01-useState/01-error-example.jsx
+++ 03-advanced-react/src/tutorial/01-useState/01-error-example.jsx
@@ -1,5 +1,24 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  // Initiating the counter
+  let count = 0;
+
+  // Creating a button click handler
+  const handleClick = () => {
+    count = count + 1;
+    console.log(count);
+  };
+
+  // Returned JSX
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={handleClick} className="btn">
+        increase
+      </button>
+    </div>
+  );
 };
 
 export default ErrorExample;
+
+// Works but nothing happens on the screen as the app is not re-rendered
