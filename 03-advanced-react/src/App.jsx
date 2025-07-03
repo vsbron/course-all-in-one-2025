00b21d32d7906ea00d57e54addbe932bebcf1134
vsbron <vsbron@gmail.com>
2025-07-03 13:21:44 +0300

    03-advanced-react - Add correct basic state example with the counter

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,11 @@
-import ErrorExample from "./tutorial/01-useState/01-error-example";
+import UseStateBasics from "./tutorial/01-useState/02-useState-basics";
 
 function App() {
   // Returned JSX
   return (
     <div className="container">
       <h2>Advanced React</h2>
-      <ErrorExample />
+      <UseStateBasics />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/01-useState/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/02-useState-basics.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // Setting the state for the counter
+  const [count, setCount] = useState(0);
+
+  // Creating a button click handler
+  const handleClick = () => {
+    setCount((c) => c + 1);
+  };
+
+  // Returned JSX
+  return (
+    <div>
+      <h4>You clicked {count} times!</h4>
+      <button onClick={handleClick} className="btn">
+        Click Me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
