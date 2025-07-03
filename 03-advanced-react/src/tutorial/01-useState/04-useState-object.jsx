2ae32c49c2abf8d9ee46f2b43dd673204fad85d2
vsbron <vsbron@gmail.com>
2025-07-03 14:46:11 +0300

    03-advanced-react - Change the state for a single object

--- 03-advanced-react/src/tutorial/01-useState/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/04-useState-object.jsx
@@ -2,23 +2,23 @@ import { useState } from "react";
 
 const UseStateObject = () => {
   // Initiating the state
-  const [name, setName] = useState("Eugene");
-  const [age, setAge] = useState(29);
-  const [hobby, setHobby] = useState("Wrestling");
+  const [person, setPerson] = useState({
+    name: "Eugene",
+    age: 29,
+    hobby: "Wrestling",
+  });
 
   // Creating the click handler
   const handleClick = () => {
-    setName("Boris");
-    setAge(37);
-    setHobby("React");
+    setPerson({ name: "Boris", age: 37, hobby: "React" });
   };
 
   // Returned JSX
   return (
     <div>
-      <h4>{name}</h4>
-      <h4>{age}</h4>
-      <h4>{hobby}</h4>
+      <h4>{person.name}</h4>
+      <h4>{person.age}</h4>
+      <h4>{person.hobby}</h4>
       <button onClick={handleClick}>Set changes</button>
     </div>
   );
