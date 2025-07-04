fa5605403caee6e4e17c2a123e3bd9345a930e6d
vsbron <vsbron@gmail.com>
2025-07-04 18:01:49 +0300

    03-advanced-react - Basic example of miltiple returns

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,11 @@
-import FetchData from "./tutorial/02-useEffect/04-fetch-data";
+import MultipleReturnsBasics from "./tutorial/03-conditional-rendering/01-multiple-returns-basics";
 
 function App() {
   // Returned JSX
   return (
     <div className="container">
       <h2>Advanced React</h2>
-      <FetchData />
+      <MultipleReturnsBasics />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/03-conditional-rendering/01-multiple-returns-basics.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/01-multiple-returns-basics.jsx
@@ -1,6 +1,26 @@
 import { useEffect, useState } from "react";
 
 const MultipleReturnsBasics = () => {
-  return <h2>Multiple Returns Basics</h2>;
+  // Creating the state for loading
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Creating the useEffect function
+  useEffect(() => {
+    // Change the isLoading state after 3 seconds
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    // Cleanup function
+    return () => {
+      clearTimeout(loadingTimeout);
+    };
+  }, []);
+
+  // Returned JSX
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  } else return <h2>Multiple Returns Basics</h2>;
 };
+
 export default MultipleReturnsBasics;
