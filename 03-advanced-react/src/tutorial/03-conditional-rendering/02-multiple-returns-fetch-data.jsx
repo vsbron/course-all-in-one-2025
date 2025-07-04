14395d1acc93d8664c97790d1b84ee7bec757378
vsbron <vsbron@gmail.com>
2025-07-04 18:17:55 +0300

    03-advanced-react - Improve the code with loading and error states

--- 03-advanced-react/src/tutorial/03-conditional-rendering/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/02-multiple-returns-fetch-data.jsx
@@ -3,8 +3,10 @@ import { useEffect, useState } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  // Creating the state for the user
-  const [user, setUser] = useState(null);
+  // Creating the state for the user, loading and error
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   // Creating the useEffect function
   useEffect(() => {
@@ -15,12 +17,23 @@ const MultipleReturnsFetchData = () => {
     // The fetch function
     const fetchData = async () => {
       try {
+        // Enabling loading state
+        setIsLoading(true);
+
+        // Fetching the data
         const response = await fetch(url, { signal });
         const data = await response.json();
-        console.log(data);
+
+        // Disabling the error, setting the user
+        setIsError(false);
         setUser(data);
       } catch {
+        // If error, enabling error, logging the message
+        setIsError(true);
         console.log("There was an error while fetching data");
+      } finally {
+        // Disabling the loading state
+        setIsLoading(false);
       }
     };
 
@@ -32,6 +45,16 @@ const MultipleReturnsFetchData = () => {
   }, []);
 
   // Returned JSX
-  return <h2>Fetch Data</h2>;
+  if (isLoading) {
+    return <h2>Loading</h2>;
+  } else if (isError) {
+    return <h2>There was an error while fetching data</h2>;
+  } else
+    return (
+      <>
+        <h2>Fetch Data</h2>
+        <div>{user.login}</div>
+      </>
+    );
 };
 export default MultipleReturnsFetchData;
