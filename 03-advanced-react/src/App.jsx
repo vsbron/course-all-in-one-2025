617dc556ec06db6e27b751f7d3b981dc316c9d05
vsbron <vsbron@gmail.com>
2025-07-06 12:05:48 +0300

    03-advanced-react - Complete the user challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,11 @@
-import ToggleChallenge from "./tutorial/03-conditional-rendering/06-toggle-challenge";
+import UserChallenge from "./tutorial/03-conditional-rendering/07-user-challenge";
 
 function App() {
   // Returned JSX
   return (
     <div className="container">
       <h2>Advanced React</h2>
-      <ToggleChallenge />
+      <UserChallenge />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/03-conditional-rendering/07-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/07-user-challenge.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  // Creating user state
+  const [user, setUser] = useState(null);
+
+  // Creating auth functions (kinda)
+  const logIn = () => {
+    setUser({ name: "John Doe", age: 25, type: "Student" });
+  };
+  const logOut = () => {
+    setUser(null);
+  };
+
+  // Returned JSX
+  return (
+    <div>
+      <h2>User challenge</h2>
+
+      {user ? (
+        // Display welcome message if user is logged in
+        <div>
+          <div>Hello there {user.name}</div>
+          <button className="btn" onClick={logOut}>
+            Log out
+          </button>
+        </div>
+      ) : (
+        // Display log in prompt if user is logged out
+        <div>
+          <div>Please log in</div>
+          <button className="btn" onClick={logIn}>
+            Log in
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
