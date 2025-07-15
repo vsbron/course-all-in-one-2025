4607885deb4558e2e9b95ab773fbb7f52ce585f8
vsbron <vsbron@gmail.com>
2025-07-15 10:58:51 +0300

    04-fundamental-projects - 01 - Add clear button

--- 04-fundamental-projects/01-birthday-buddy/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/src/App.jsx
@@ -8,12 +8,20 @@ const App = () => {
   // Creating the state value for the people
   const [people, setPeople] = useState(data);
 
+  // Creating the clear list function
+  const clearList = () => {
+    setPeople([]);
+  };
+
   // Returned JSX
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button type="button" className="btn btn-block" onClick={clearList}>
+          Clear all
+        </button>
       </section>
     </main>
   );
