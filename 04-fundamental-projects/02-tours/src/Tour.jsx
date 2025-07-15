c4412182f49d73abff520772f392d4606d319fdf
vsbron <vsbron@gmail.com>
2025-07-15 13:38:34 +0300

    04-fundamental-projects - 02 - Add show more/less functionality

--- 04-fundamental-projects/02-tours/src/Tour.jsx
+++ 04-fundamental-projects/02-tours/src/Tour.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 function Tour({ id, name, info, image, price, removeTour }) {
+  // Create the state value for expanded text
+  const [readMore, setReadMore] = useState(false);
+
+  // Toggle text handler
+  const toggleText = () => {
+    setReadMore((iE) => !iE);
+  };
+
+  // Modify the text based on the readMore state
+  const modifiedInfo = readMore ? info : `${info.substring(0, 200)}...`;
+
   // Returned JSX
   return (
     <article className="single-tour">
@@ -6,7 +19,13 @@ function Tour({ id, name, info, image, price, removeTour }) {
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p>
+          {modifiedInfo}
+          <br />
+          <button type="button" className="info-btn" onClick={toggleText}>
+            {readMore ? "Show less" : "Show more"}
+          </button>
+        </p>
         <button
           className="btn btn-block delete-btn"
           onClick={() => removeTour(id)}
