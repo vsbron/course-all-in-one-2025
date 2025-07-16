a1896ce0cb5af2b962f9ae3b1bfa9ea8e2751e17
vsbron <vsbron@gmail.com>
2025-07-16 10:07:20 +0300

    04-fundamental-projects - 03 - initial setup

--- 04-fundamental-projects/03-reviews/src/App.jsx
+++ 04-fundamental-projects/03-reviews/src/App.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
+import people from "./data";
+
 const App = () => {
+  // Creating state value for the people index
+  const [index, setIndex] = useState(0);
+
+  // Destructuring the person data
+  const { name, job, image, text } = people[index];
+
+  console.log(name);
+
+  // Returned JSX
   return <h2>Reviews Starter</h2>;
 };
 export default App;
