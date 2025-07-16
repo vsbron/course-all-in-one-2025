c92912b0e28addcaf05dd2734078d1f62dde8a56
vsbron <vsbron@gmail.com>
2025-07-16 11:39:51 +0300

    04-fundamental-projects - 04 - Add data toggle to SingleQuestion component - PROJECT COMPLETED

--- 04-fundamental-projects/04-accordion/src/SingleQuestion.jsx
+++ 04-fundamental-projects/04-accordion/src/SingleQuestion.jsx
@@ -5,13 +5,21 @@ function SingleQuestion({ title, info }) {
   // Creating state value for show info flag
   const [showInfo, setShowInfo] = useState(false);
 
+  // Create the function to toggle the showInfo
+  const toggleShowInfo = () => {
+    setShowInfo((sI) => !sI);
+  };
+
   // Returned JSX
   return (
     <article className="question">
       <header>
         <h5>{title}</h5>
+        <button className="question-btn" onClick={toggleShowInfo}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
       </header>
-      <p>{info}</p>
+      {showInfo && <p>{info}</p>}
     </article>
   );
 }
