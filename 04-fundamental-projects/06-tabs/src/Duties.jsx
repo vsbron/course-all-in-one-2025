ed06a5f17542a8da7c029a878bf2e1373b22539b
vsbron <vsbron@gmail.com>
2025-07-17 12:27:46 +0300

    04-fundamnetal-projects - 06 - Install react-icons and uuid packages. Set up Duties component data display

--- 04-fundamental-projects/06-tabs/src/Duties.jsx
+++ 04-fundamental-projects/06-tabs/src/Duties.jsx
@@ -1,5 +1,18 @@
+import { FaAngleDoubleRight } from "react-icons/fa";
+import { v4 as uuid4 } from "uuid";
+
 function Duties({ duties }) {
-  return <div>Duties</div>;
+  // Returned JSX
+  return (
+    <div>
+      {duties.map((duty, i) => (
+        <div key={uuid4()} className="job-desc">
+          <FaAngleDoubleRight className="job-icon" />
+          <p>{duty}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Duties;
