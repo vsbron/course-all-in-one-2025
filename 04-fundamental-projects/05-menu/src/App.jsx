502ffcc07156e4c2293b773a2004954b66f50049
vsbron <vsbron@gmail.com>
2025-07-17 11:44:41 +0300

    04-fundamental-projects - 05 - Add a Category filter functionality - PROJECT COMPLETED

--- 04-fundamental-projects/05-menu/src/App.jsx
+++ 04-fundamental-projects/05-menu/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 import menu from "./data";
 
+import Categories from "./Categories";
 import Menu from "./Menu";
 import Title from "./Title";
-import Categories from "./Categories";
 
 // Getting the list with uniques menu items categories
 const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
@@ -12,14 +12,20 @@ const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 const App = () => {
   // Creating state value for menu items
   const [menuItems, setMenuItems] = useState(menu);
-  const [categories, setCategories] = useState(allCategories);
+
+  // Create the filtered items based on a chosen category
+  const filteredItems = (cat) => {
+    setMenuItems(
+      cat !== "all" ? menu.filter((item) => item.category === cat) : menu
+    );
+  };
 
   // Returned JSX
   return (
     <main>
       <section className="menu">
         <Title text="Our menu" />
-        <Categories categories={categories} />
+        <Categories categories={allCategories} filteredItems={filteredItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- 04-fundamental-projects/05-menu/src/Categories.jsx
+++ 04-fundamental-projects/05-menu/src/Categories.jsx
@@ -1,9 +1,14 @@
-function Categories({ categories }) {
+function Categories({ categories, filteredItems }) {
   // Returned JSX
   return (
     <div className="btn-container">
       {categories.map((cat) => (
-        <button type="button" className="btn" key={cat}>
+        <button
+          type="button"
+          className="btn"
+          key={cat}
+          onClick={() => filteredItems(cat)}
+        >
           {cat}
         </button>
       ))}
