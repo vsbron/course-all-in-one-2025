37aa1807421423dedff2e8d8ddcc6d32930019d1
vsbron <vsbron@gmail.com>
2025-07-17 11:26:21 +0300

    04-fundamental-projects - 05 - Set the list of unique menu items categories

--- 04-fundamental-projects/05-menu/src/App.jsx
+++ 04-fundamental-projects/05-menu/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
+import menu from "./data";
+
+import Menu from "./Menu";
 import Title from "./Title";
 
-import menu from "./data";
-import Menu from "./Menu";
+// Getting the list with uniques menu items categories
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 
 const App = () => {
   // Creating state value for menu items
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
 
   // Returned JSX
   return (
