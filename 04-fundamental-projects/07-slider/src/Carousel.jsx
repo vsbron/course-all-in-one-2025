e9cc83ba3111c09a0928d22622b37e4257e75ef6
vsbron <vsbron@gmail.com>
2025-07-22 15:50:27 +0300

    04-fundamental-projects - 07 - Import the long list of sliders. Place them horizontally for the slider

--- 04-fundamental-projects/07-slider/src/Carousel.jsx
+++ 04-fundamental-projects/07-slider/src/Carousel.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
-import { shortList, longList } from "./data";
+import { longList } from "./data";
 
 function Carousel() {
   // Create state value for the people
-  const [people, setPeople] = useState(shortList);
+  const [people, setPeople] = useState(longList);
 
   // Creating handler function for changing the slides
   const prevSlide = () => {};
@@ -15,8 +15,12 @@ function Carousel() {
   // Returned JSX
   return (
     <section className="slider-container">
-      {people.map(({ id, image, name, title, quote }) => (
-        <article className="slide" key={id}>
+      {people.map(({ id, image, name, title, quote }, index) => (
+        <article
+          className="slide"
+          style={{ transform: `translateX(${100 * index}%)` }}
+          key={id}
+        >
           <img src={image} alt={name} className="person-img" />
           <h5 className="name">{name}</h5>
           <p className="title">{title}</p>
