30d35e3f4e78d8173de69f4abdf3a9669ca5ffc6
vsbron <vsbron@gmail.com>
2025-07-23 12:49:01 +0300

    04-fundamental-projects - 09 - Add new state to APp with the array of tints and shades of default colot

--- 04-fundamental-projects/09-color-generator/src/App.jsx
+++ 04-fundamental-projects/09-color-generator/src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import Values from "values.js";
+
 import ColorList from "./ColorList";
 import Form from "./Form";
 
 function App() {
+  // Create state value for the colors
+  const [colors, setColors] = useState(new Values("#f15025").all(10));
+
   // Returned JSX
   return (
     <main>
