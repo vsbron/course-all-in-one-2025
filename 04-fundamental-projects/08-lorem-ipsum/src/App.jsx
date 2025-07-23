55f20a12acf803cd159375b8b683fe66e58d9e6a
vsbron <vsbron@gmail.com>
2025-07-23 12:12:58 +0300

    04-fundamental-projects - 07 - Set up the main fuinctionality

--- 04-fundamental-projects/08-lorem-ipsum/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/src/App.jsx
@@ -11,11 +11,19 @@ function App() {
   const [count, setCount] = useState(MIN_PARAGRAPHS);
   const [text, setText] = useState([]);
 
+  const handleSubmit = (e) => {
+    // Prevent default behavior
+    e.preventDefault();
+
+    // Getting the needed amount of paragraphs from data
+    setText(data.slice(0, parseInt(count)));
+  };
+
   // Returned JSX
   return (
     <section className="section-center">
       <h4>Tired of boring lorem ipsum?</h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">Paragraphs:</label>
         <input
           type="number"
@@ -31,6 +39,11 @@ function App() {
           Generate
         </button>
       </form>
+      <article className="lorem-text">
+        {text.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </article>
     </section>
   );
 }
