b31ed471494edfd1e61d16da95b4bac509497fa0
vsbron <vsbron@gmail.com>
2025-07-23 12:05:39 +0300

    04-fundamental-projects - 07 - Initialize project. Set the App component with the state and UI

--- 04-fundamental-projects/08-lorem-ipsum/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/src/App.jsx
@@ -1,4 +1,38 @@
-const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
-};
+import { useState } from "react";
+
+import data from "./data";
+
+// Paragraphs limit const
+const MIN_PARAGRAPHS = 1;
+const MAX_PARAGRAPHS = 8;
+
+function App() {
+  // Creating state value for the count and text
+  const [count, setCount] = useState(MIN_PARAGRAPHS);
+  const [text, setText] = useState([]);
+
+  // Returned JSX
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min={MIN_PARAGRAPHS}
+          max={MAX_PARAGRAPHS}
+          step={1}
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+    </section>
+  );
+}
+
 export default App;
