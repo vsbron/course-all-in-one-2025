ce557673da05382b21d1979fde05057a0c82eab8
vsbron <vsbron@gmail.com>
2025-07-24 11:08:57 +0300

    04-fundamental-projects - 10 - Item List is now being show. Items can be marked as completed

--- 04-fundamental-projects/10-grocery-bud/src/SingleItem.jsx
+++ 04-fundamental-projects/10-grocery-bud/src/SingleItem.jsx
@@ -1,6 +1,30 @@
-function SingleItem() {
+import { useState } from "react";
+
+function SingleItem({ item, removeItem }) {
+  // Create state value for item's checked status
+  const [isChecked, setIsChecked] = useState(item.completed);
+
   // Returned JSX
-  return <div>Single Item</div>;
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked((iC) => !iC)}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: isChecked && "line-through",
+        }}
+      >
+        {item.name}
+      </p>
+      <button className="btn remove-btn" type="button">
+        Delete
+      </button>
+    </div>
+  );
 }
 
 export default SingleItem;
