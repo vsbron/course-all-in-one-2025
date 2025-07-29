20fef7c51a3b5a41381c67a301f30f8c98f6b7c2
vsbron <vsbron@gmail.com>
2025-07-29 18:48:01 +0300

    04-fundamental-projects - 12 - Set up the Sidebar component

--- 04-fundamental-projects/12-sidebar/src/Sidebar.jsx
+++ 04-fundamental-projects/12-sidebar/src/Sidebar.jsx
@@ -1,6 +1,42 @@
+import { FaTimes } from "react-icons/fa";
+
+import { useAppContext } from "./AppContext";
+
+import { social, links } from "./data";
+import logo from "./logo.svg";
+
 function Sidebar() {
+  // Getting the isSidebarOpen and closeSidebar from the Context API
+  const { isSidebarOpen, closeSidebar } = useAppContext();
+
   // Returned JSX
-  return <div>Sidebar</div>;
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="Coding addict" className="logo" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map(({ id, url, text, icon }) => (
+          <li key={id}>
+            <a href={url}>
+              {icon}
+              {text}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className="social-links">
+        {social.map(({ id, url, icon }) => (
+          <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 export default Sidebar;
