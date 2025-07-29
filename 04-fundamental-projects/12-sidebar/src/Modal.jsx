1a4e8eb8d12f313704c7f719525195e5fc77de7a
vsbron <vsbron@gmail.com>
2025-07-29 18:41:23 +0300

    04-fundamental-projects - 12 - Set up the Modal component and CSS

--- 04-fundamental-projects/12-sidebar/src/Modal.jsx
+++ 04-fundamental-projects/12-sidebar/src/Modal.jsx
@@ -1,6 +1,22 @@
+import { FaTimes } from "react-icons/fa";
+
+import { useAppContext } from "./AppContext";
+
 function Modal() {
+  // Getting the isModalOpen and closeModal from the Context API
+  const { isModalOpen, closeModal } = useAppContext();
+
   // Returned JSX
-  return <div>Modal</div>;
+  return (
+    <div className={`modal-overlay ${isModalOpen ? "show-modal" : ""}`}>
+      <div className="modal-container">
+        <h3>Modal content</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
