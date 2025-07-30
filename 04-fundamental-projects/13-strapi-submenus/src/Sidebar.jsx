99f79ca2ebecba5fd7ac46fa0768fd9064ff063a
vsbron <vsbron@gmail.com>
2025-07-30 12:41:38 +0300

    04-fundamental-projects - 13 - Set up the Sidebar component

--- 04-fundamental-projects/13-strapi-submenus/src/Sidebar.jsx
+++ 04-fundamental-projects/13-strapi-submenus/src/Sidebar.jsx
@@ -1,6 +1,37 @@
+import { FaTimes } from "react-icons/fa";
+
+import { useAppContext } from "./AppContext";
+
+import sublinks from "./data";
+
 function Sidebar() {
+  // Get the values from Context API
+  const { isSidebarOpen, closeSidebar } = useAppContext();
+
   // Returned JSX
-  return <h2>Sidebar</h2>;
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map(({ pageId, page, links }) => (
+            <article key={pageId}>
+              <h4>{page}</h4>
+              <div className="sidebar-sublinks">
+                {links.map(({ url, icon, label, id }) => (
+                  <a href={url} key={id}>
+                    {icon} {label}
+                  </a>
+                ))}
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </aside>
+  );
 }
 
 export default Sidebar;
