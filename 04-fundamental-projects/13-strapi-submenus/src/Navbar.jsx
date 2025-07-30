955b4acc9858530eb2db422e2ec38d4d45297227
vsbron <vsbron@gmail.com>
2025-07-30 12:07:19 +0300

    04-fundamental-projects - 13 - Set up the Navbar component

--- 04-fundamental-projects/13-strapi-submenus/src/Navbar.jsx
+++ 04-fundamental-projects/13-strapi-submenus/src/Navbar.jsx
@@ -1,6 +1,20 @@
+import { FaBars } from "react-icons/fa";
+import { useAppContext } from "./AppContext";
+
 function Navbar() {
+  // Get the values from Context API
+  const { openSidebar } = useAppContext();
+
   // Returned JSX
-  return <h2>Navbar</h2>;
+  return (
+    <nav className="nav-center">
+      <h3 className="logo">Strapi</h3>
+      <button className="toggle-btn" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      {/* NAV LINKS LATER */}
+    </nav>
+  );
 }
 
 export default Navbar;
