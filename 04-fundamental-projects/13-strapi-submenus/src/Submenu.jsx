37638ad617966f494b7f149fb2833a59e98525f7
vsbron <vsbron@gmail.com>
2025-07-31 13:28:44 +0300

    04-fundamental=projects - 13 - Set up Submenu component

--- 04-fundamental-projects/13-strapi-submenus/src/Submenu.jsx
+++ 04-fundamental-projects/13-strapi-submenus/src/Submenu.jsx
@@ -1,6 +1,33 @@
+import { useAppContext } from "./AppContext";
+
+import sublinks from "./data";
+
 function Submenu() {
+  // Get the values from Context API
+  const { pageId } = useAppContext();
+
+  // Getting the correct page from the sublinks
+  const currentPage = sublinks.find((link) => link.pageId === pageId);
+
   // Returned JSX
-  return <h2>Submenu</h2>;
+  return (
+    <div className="submenu">
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
+        }}
+      >
+        {currentPage?.links?.map(({ id, url, label, icon }) => (
+          <a key={id} href={url}>
+            {icon} {label}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Submenu;
