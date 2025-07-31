8f866777b38c90f00526a02a422561333128db11
vsbron <vsbron@gmail.com>
2025-07-31 14:28:15 +0300

    04-fundamental=projects - 13 - Imporove the close submenu functionality. Exiting from top not closing it

--- 04-fundamental-projects/13-strapi-submenus/src/Submenu.jsx
+++ 04-fundamental-projects/13-strapi-submenus/src/Submenu.jsx
@@ -1,17 +1,41 @@
+import { useRef } from "react";
 import { useAppContext } from "./AppContext";
 
 import sublinks from "./data";
 
 function Submenu() {
   // Get the values from Context API
-  const { pageId } = useAppContext();
+  const { pageId, setPageId } = useAppContext();
 
   // Getting the correct page from the sublinks
   const currentPage = sublinks.find((link) => link.pageId === pageId);
 
+  // Setting hte ref for the sub menu
+  const submenuContainer = useRef(null);
+
+  // Create handler when mouse leaves submenu function
+  const handleMouseLeave = (event) => {
+    // Getting the submenu element
+    const submenu = submenuContainer.current;
+
+    // Getting the bounding client rectangle and Y coordinate of the event
+    const { top } = submenu.getBoundingClientRect();
+    const { clientY } = event;
+
+    // Early exit if we moved cursor from top
+    if (clientY < top) return;
+
+    // Close the submenu
+    setPageId(null);
+  };
+
   // Returned JSX
   return (
-    <div className={`submenu ${currentPage ? "show-submenu" : ""}`}>
+    <div
+      className={`submenu ${currentPage ? "show-submenu" : ""}`}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
