0c3c6c6679566c6ac31519e5c81ba933a9c4536c
vsbron <vsbron@gmail.com>
2025-07-31 14:15:17 +0300

    04-fundamental=projects - 13 - Set up the closing submenu functionality

--- 04-fundamental-projects/13-strapi-submenus/src/Navbar.jsx
+++ 04-fundamental-projects/13-strapi-submenus/src/Navbar.jsx
@@ -6,11 +6,18 @@ import NavLinks from "./NavLinks";
 
 function Navbar() {
   // Get the values from Context API
-  const { openSidebar } = useAppContext();
+  const { openSidebar, setPageId } = useAppContext();
+
+  // Create the mouse over nav handle function
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
 
   // Returned JSX
   return (
-    <nav className="nav">
+    <nav className="nav" onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <h3 className="logo">Strapi</h3>
         <button className="toggle-btn" onClick={openSidebar}>
