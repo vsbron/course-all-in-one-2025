17b02c6bdb00c819e8ae3a17f7534859e1d2c439
vsbron <vsbron@gmail.com>
2025-07-31 14:30:16 +0300

    04-fundamental-projects - 14 - Initial set up

--- 04-fundamental-projects/14-cart/src/App.jsx
+++ 04-fundamental-projects/14-cart/src/App.jsx
@@ -1,8 +1,9 @@
 // components
-import Navbar from './Navbar';
-import CartContainer from './CartContainer';
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
 
 function App() {
+  // Returned JSX
   return (
     <main>
       <Navbar />
