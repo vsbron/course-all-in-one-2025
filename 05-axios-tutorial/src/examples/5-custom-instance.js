1c0fb0c5f679d00317523f81a1d8c7bb69af7c8f
vsbron <vsbron@gmail.com>
2025-08-04 19:42:45 +0300

    05-axios-tutorial - Set up an example with Custom instance

--- 05-axios-tutorial/src/examples/5-custom-instance.js
+++ 05-axios-tutorial/src/examples/5-custom-instance.js
@@ -1,16 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import axios from "axios";
 
-const randomUserUrl = 'https://randomuser.me/api';
+import authFetch from "../axios/custom";
+
+// Set the URL for fetching
+const randomUserUrl = "https://randomuser.me/api";
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try {
+      const resp1 = await authFetch("react-store-products");
+      const resp2 = await axios(randomUserUrl);
+      console.log(resp1);
+      console.log(resp2);
+    } catch (error) {}
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>custom instance</h2>;
+  // Returned JSX
+  return <h2 className="text-center">custom instance</h2>;
 };
 export default CustomInstance;
