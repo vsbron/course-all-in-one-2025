57c52dcd5c8c741b29711925c9bb91fc0114ce68
vsbron <vsbron@gmail.com>
2025-08-04 18:09:42 +0300

    05-axios-tutorial - Set up basic axios request example

--- 05-axios-tutorial/src/App.js
+++ 05-axios-tutorial/src/App.js
@@ -1,8 +1,13 @@
-import Title from './components/Title';
+import Title from "./components/Title";
+import Setup from "./examples/1-first-request"
+
+
 function App() {
+  // Returned JSX
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
--- 05-axios-tutorial/src/examples/1-first-request.js
+++ 05-axios-tutorial/src/examples/1-first-request.js
@@ -1,12 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+
+import axios from "axios";
+
 // limit, if 429 wait for 15 min and try again
-const url = 'https://www.course-api.com/react-store-products';
+const url = "https://www.course-api.com/react-store-products";
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      // Fetching the response data
+      const response = await axios(url);
+
+      // Get the actual data and log it
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   useEffect(() => {
-    console.log('first axios request');
+    fetchData();
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  // Returned JSX
+  return <h2 className="text-center">first request</h2>;
 };
 export default FirstRequest;
