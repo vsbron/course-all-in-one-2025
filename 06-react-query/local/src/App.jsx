bc036374ac6f39a51d2586890321ed47afcfc517
vsbron <vsbron@gmail.com>
2025-08-07 12:47:35 +0300

    Install libraries

--- 06-react-query/local/src/App.jsx
+++ 06-react-query/local/src/App.jsx
@@ -1,22 +1,30 @@
-import { ToastContainer } from 'react-toastify';
-import { nanoid } from 'nanoid';
-import Form from './Form';
-import Items from './Items';
-import { useState } from 'react';
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import { ToastContainer } from "react-toastify";
+
+import Form from "./Form";
+import Items from "./Items";
+
+// Default local data
 const defaultItems = [
-  { id: nanoid(), title: 'walk the dog', isDone: false },
-  { id: nanoid(), title: 'wash dishes', isDone: false },
-  { id: nanoid(), title: 'drink coffee', isDone: true },
-  { id: nanoid(), title: 'take a nap', isDone: false },
+  { id: nanoid(), title: "walk the dog", isDone: false },
+  { id: nanoid(), title: "wash dishes", isDone: false },
+  { id: nanoid(), title: "drink coffee", isDone: true },
+  { id: nanoid(), title: "take a nap", isDone: false },
 ];
-const App = () => {
+
+function App() {
+  // Create state value for items
   const [items, setItems] = useState(defaultItems);
+
+  // Returned JSX
   return (
-    <section className='section-center'>
-      <ToastContainer position='top-center' />
+    <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form />
       <Items items={items} />
     </section>
   );
-};
+}
+
 export default App;
