6236e6c8f6d16737204dff7d98fd141a7adf2103
vsbron <vsbron@gmail.com>
2025-08-07 13:24:33 +0300

    06-reqct-query - Install and set up React Query provider

--- 06-react-query/local/src/main.jsx
+++ 06-react-query/local/src/main.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import App from "./App";
+
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+
+// Create query client
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
