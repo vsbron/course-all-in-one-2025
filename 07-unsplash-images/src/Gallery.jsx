dfe06273476c3b16e0c74b82a56e31deddef4ded
vsbron <vsbron@gmail.com>
2025-08-10 10:52:57 +0300

    07-unsplash-images - Set up the images display after a successful fetch

--- 07-unsplash-images/src/Gallery.jsx
+++ 07-unsplash-images/src/Gallery.jsx
@@ -6,18 +6,43 @@ const url = "https://api.unsplash.com/search/photos?client_id=";
 
 function Gallery() {
   // Fetching the data
-  const response = useQuery({
+  const { isLoading, data, error } = useQuery({
     queryKey: ["images"],
     queryFn: async () => {
       const result = await axios.get(
-        `${url}${import.meta.env.VITE_UNSPLASH_PUBLIC_KEY}&query=office`
+        `${url}${import.meta.env.VITE_UNSPLASH_PUBLIC_KEY}&query=Wrestling`
       );
       return result.data;
     },
   });
 
+  // Guard clauses
+  if (isLoading || error || data.results.length < 1)
+    return (
+      <section className="image-container">
+        <h4>
+          {isLoading
+            ? "Loading..."
+            : error
+            ? "There was an error"
+            : "No images found"}
+        </h4>
+      </section>
+    );
+
   // Returned JSX
-  return <div>Gallery</div>;
+  return (
+    <section className="image-container">
+      {data.results.map((image) => (
+        <img
+          src={image?.urls?.regular}
+          key={image.id}
+          className="img"
+          alt={image.alt_description}
+        />
+      ))}
+    </section>
+  );
 }
 
 export default Gallery;
