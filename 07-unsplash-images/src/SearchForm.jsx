708704931f863faa4b3fddcb7b65b23119a33148
vsbron <vsbron@gmail.com>
2025-08-10 10:13:17 +0300

    07-unsplash-images - Initial set up of Search Form component

--- 07-unsplash-images/src/SearchForm.jsx
+++ 07-unsplash-images/src/SearchForm.jsx
@@ -1,6 +1,35 @@
 function SearchForm() {
+  // Form submit handler
+  const submitHandler = (e) => {
+    // Prevent default behavior
+    e.preventDefault();
+
+    // Get the input value
+    const searchValue = e.target.elements.search.value;
+
+    // Guard clause
+    if (!searchValue) return;
+
+    console.log(searchValue);
+  };
+
   // Returned JSX
-  return <div>Search Form</div>;
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={submitHandler}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="Cat"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm;
