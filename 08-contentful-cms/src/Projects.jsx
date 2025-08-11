1ad437f4dfb57c8afed33c51b4cdab4c3d5cdf1c
vsbron <vsbron@gmail.com>
2025-08-11 12:15:59 +0300

    Set up the data fetch and refactor to simpler object to work with

--- 08-contentful-cms/src/Projects.jsx
+++ 08-contentful-cms/src/Projects.jsx
@@ -1,6 +1,13 @@
 import "./fetchProjects";
+import { useFetchProjects } from "./fetchProjects";
 
 function Projects() {
+  // Get the isLoading state and the projects from the custom hook
+  const { isLoading, projects } = useFetchProjects();
+
+  // Log the result
+  console.log(projects);
+
   // Returned JSX
   return (
     <section>
--- 08-contentful-cms/src/fetchProjects.jsx
+++ 08-contentful-cms/src/fetchProjects.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { createClient } from "contentful";
 
 // Create Contentful client
@@ -7,6 +8,42 @@ const client = createClient({
   accessToken: import.meta.env.VITE_CONTENTFUL_DELIVERY_ACCESS,
 });
 
-client
-  .getEntries({ content_type: "projects" })
-  .then((response) => console.log(response));
+export const useFetchProjects = () => {
+  // Create state value for isLoading and projects
+  const [isLoading, setIsLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  // Async function that does the data fetch
+  const getData = async () => {
+    try {
+      // Get the data
+      const response = await client.getEntries({ content_type: "projects" });
+      const fetchedProjects = response.items.map((item) => {
+        // Get the item data
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+
+        // Return the data as the object
+        return { title, url, id, img };
+      });
+
+      // Update the state
+      setProjects(fetchedProjects);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      // Disable loading state
+      setIsLoading(false);
+    }
+  };
+
+  // useEffect function that
+  useEffect(() => {
+    // Call the fetching function
+    getData();
+  }, []);
+
+  // Return the projects and loading state
+  return { projects, isLoading };
+};
