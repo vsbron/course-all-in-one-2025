5e41a0ffa00eb344f4266d0b88f203f523cd5c50
vsbron <vsbron@gmail.com>
2025-08-12 10:39:37 +0300

    09-mixmaster - Set up basic react router code

--- 09-mixmaster/src/App.jsx
+++ 09-mixmaster/src/App.jsx
@@ -1,9 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Create react router
+const router = createBrowserRouter([
+  { path: "/", element: <h2>Home</h2> },
+  { path: "/about", element: <h2>About</h2> },
+  { path: "/temp", element: <h2>Temp</h2> },
+]);
+
 function App() {
   // Returned JSX
   return (
-    <main>
-      <h1>Mixmaster</h1>
-    </main>
+    <RouterProvider router={router}>
+      <main>
+        <h1>MixMaster</h1>
+      </main>
+    </RouterProvider>
   );
 }
 
