ab1bf47c5e3b496369555ce8e15a9a10238ba008
vsbron <vsbron@gmail.com>
2025-08-12 10:19:49 +0300

    08-contentful-cms - Set up the projects list

--- 08-contentful-cms/src/Projects.jsx
+++ 08-contentful-cms/src/Projects.jsx
@@ -1,17 +1,39 @@
-import "./fetchProjects";
 import { useFetchProjects } from "./fetchProjects";
 
 function Projects() {
   // Get the isLoading state and the projects from the custom hook
   const { isLoading, projects } = useFetchProjects();
 
-  // Log the result
-  console.log(projects);
+  // Guard clause
+  if (isLoading) {
+    return (
+      <section className="projects">
+        <h2>Loading...</h2>
+      </section>
+    );
+  }
 
   // Returned JSX
   return (
-    <section>
-      <h2>Projects</h2>
+    <section className="projects">
+      <div className="title">
+        <h2>Projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.map(({ id, img, url, title }) => (
+          <a
+            key={id}
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+            className="project"
+          >
+            <img src={img} alt={title} className="img" />
+            <h5>{title}</h5>
+          </a>
+        ))}
+      </div>
     </section>
   );
 }
