46a204c77ef6412ee3e6b314f707544e57d3aa4f
vsbron <vsbron@gmail.com>
2025-08-13 10:50:34 +0300

    09-mixmaster - Set up the Loader component while the Drinks are loading

--- 09-mixmaster/src/pages/HomeLayout.jsx
+++ 09-mixmaster/src/pages/HomeLayout.jsx
@@ -1,14 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
 
 function HomeLayout() {
+  // Checking the loading state using Navigation hook
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   // Returned JSX
   return (
     <>
       <Navbar />
       <section className="page">
-        <Outlet />
+        {isLoading ? <div className="loading" /> : <Outlet />}
       </section>
     </>
   );
