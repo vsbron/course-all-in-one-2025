f81529f9bb0079ae28c69dedf9d23f8aec2170b5
vsbron <vsbron@gmail.com>
2025-08-13 11:09:45 +0300

    09-mixmaster - Set up the individual drink page

--- 09-mixmaster/src/pages/Cocktail.jsx
+++ 09-mixmaster/src/pages/Cocktail.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
 
@@ -20,8 +20,47 @@ function Cocktail() {
   // Get the data from the loader
   const { id, data } = useLoaderData();
 
+  // Get the drink props from the fetched data
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = data.drinks[0];
+
   // Returned JSX
-  return <StyledCocktail>Cocktail</StyledCocktail>;
+  return (
+    <StyledCocktail>
+      <header>
+        <Link to="/" className="btn">
+          Back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">Name: </span> {name}
+          </p>
+          <p>
+            <span className="drink-data">Category: </span> {category}
+          </p>
+          <p>
+            <span className="drink-data">Info: </span> {info}
+          </p>
+          <p>
+            <span className="drink-data">Glass: </span> {glass}
+          </p>
+          <p>
+            <span className="drink-data">Instructions: </span> {instructions}
+          </p>
+        </div>
+      </div>
+    </StyledCocktail>
+  );
 }
 
 // Styled components
