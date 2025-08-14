6a148e627aedddfbd614b161e1bc61cf028c61c8
vsbron <vsbron@gmail.com>
2025-08-14 09:44:12 +0300

    09-mixmaster - Set up the form for the Newsletter page

--- 09-mixmaster/src/pages/Newsletter.jsx
+++ 09-mixmaster/src/pages/Newsletter.jsx
@@ -1,6 +1,58 @@
 function Newsletter() {
   // Returned JSX
-  return <h1>Newsletter</h1>;
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        Our newsletter
+      </h4>
+      {/* Name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="John"
+        />
+      </div>
+      {/* Last name */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          Last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue="Smith"
+        />
+      </div>
+      {/* Email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "0.5rem" }}
+      >
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default Newsletter;
