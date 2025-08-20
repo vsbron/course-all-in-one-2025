632acf3ad94453a2383df402145f34d4a2add65a
vsbron <vsbron@gmail.com>
2025-08-20 10:11:06 +0300

    11-redux-toolkit-tutorial - Set up CartItem component

--- 11-redux-toolkit-tutorial/src/components/CartItem.jsx
+++ 11-redux-toolkit-tutorial/src/components/CartItem.jsx
@@ -1,6 +1,26 @@
-function CartItem() {
+import { ChevronDown, ChevronUp } from "../icons";
+
+function CartItem({ id, title, price, img, amount }) {
   // Returned JSX
-  return <div>CartItem</div>;
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        <button className="remove-btn">remove</button>
+      </div>
+      <div>
+        <button className="amount-btn">
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
 }
 
 export default CartItem;
