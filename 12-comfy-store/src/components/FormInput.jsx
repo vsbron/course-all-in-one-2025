1b85d88919b2138f2ea6a73d9d093725afc61db3
vsbron <vsbron@gmail.com>
2025-08-21 12:27:09 +0300

    12-comfy-store - Set up the FormInput component

---
+++ 12-comfy-store/src/components/FormInput.jsx
@@ -0,0 +1,18 @@
+function FormInput({ label, name, type, defaultValue }) {
+  // Returned JSX
+  return (
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">{label}</span>
+      </label>
+      <input
+        type={type}
+        name={name}
+        defaultValue={defaultValue}
+        className="input input-bordered"
+      />
+    </div>
+  );
+}
+
+export default FormInput;
