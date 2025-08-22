32bb97a5177705425e466422bfddf77983fb0484
vsbron <vsbron@gmail.com>
2025-08-22 19:38:33 +0300

    12-comfy-store - Set up the Login page with base CSS

---
+++ 12-comfy-store/src/components/SubmitBtn.jsx
@@ -0,0 +1,10 @@
+function SubmitBtn({ text }) {
+  // Returned JSX
+  return (
+    <button className="btn btn-primary" type="submit">
+      {text}
+    </button>
+  );
+}
+
+export default SubmitBtn;
