df43cb096bd41a503561f4c078ab44eb02dc7447
vsbron <vsbron@gmail.com>
2025-08-23 16:25:42 +0300

    12-comfy-store - Set up the products list UX for changing the layout

--- 12-comfy-store/src/components/ProductsContainer.jsx
+++ 12-comfy-store/src/components/ProductsContainer.jsx
@@ -1,12 +1,64 @@
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { BsFillGridFill, BsList } from "react-icons/bs";
+
 import ProductsGrid from "./ProductsGrid";
 import ProductsList from "./ProductsList";
 
 function ProductsContainer() {
+  // Get the meta data from the loader hook
+  const { meta } = useLoaderData();
+  console.log(meta);
+  const totalProducts = meta.pagination.total;
+
+  // Set state value for the products layout
+  const [layout, setLayout] = useState("grid");
+
+  const setActiveStyles = (pattern) => {
+    return `text-xl btn btn-circle btn-sm ${
+      pattern === layout
+        ? "btn-primary text-primary-content"
+        : "btn-ghost text-base-content"
+    }`;
+  };
+
   // Returned JSX
   return (
     <>
-      <ProductsList />
-      <ProductsGrid />
+      {/* HEADER */}
+      <div className="flex justify-between items-center mt-8 border-b border-base-300 pb-5">
+        <h4 className="font-medium text-md">
+          {totalProducts} product{totalProducts > 1 && "s"}
+        </h4>
+        <div className="flex gap-x-2">
+          <button
+            type="button"
+            onClick={() => setLayout("grid")}
+            className={setActiveStyles("grid")}
+          >
+            <BsFillGridFill />
+          </button>
+          <button
+            type="button"
+            onClick={() => setLayout("list")}
+            className={setActiveStyles("list")}
+          >
+            <BsList />
+          </button>
+        </div>
+      </div>
+      {/* PRODUCTS */}
+      <div>
+        {totalProducts === 0 ? (
+          <h5 className="text-2xl mt-16">
+            Sorry, no products matched your search...
+          </h5>
+        ) : layout === "grid" ? (
+          <ProductsGrid />
+        ) : (
+          <ProductsList />
+        )}
+      </div>
     </>
   );
 }
