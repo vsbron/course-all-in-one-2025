bad0c58514c0c92ec9ead9d7f222452bd6912596
vsbron <vsbron@gmail.com>
2025-08-28 13:50:26 +0300

    12-comfy-store - Refactor the fetch function for Landing pahe

--- 12-comfy-store/src/pages/Landing.jsx
+++ 12-comfy-store/src/pages/Landing.jsx
@@ -1,10 +1,16 @@
 import { FeaturedProducts, Hero } from "../components";
 import { customFetch } from "../utils";
 
+// Set upt the Query function
+const featuredProductsQuery = {
+  queryKey: ["featuredProducts"],
+  queryFn: () => customFetch("/products?featured=true"),
+};
+
 // Set up the loader
 // eslint-disable-next-line react-refresh/only-export-components
 export const loader = (queryClient) => async () => {
-  const response = await customFetch("/products?featured=true");
+  const response = await queryClient.ensureQueryData(featuredProductsQuery);
   const products = response.data.data;
   return { products };
 };
