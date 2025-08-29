4c83e96330efd82aa1894d67e36dc90a7f8a0c81
vsbron <vsbron@gmail.com>
2025-08-29 16:06:00 +0300

    13-typescript-tutorial - Add Unions and any type example

--- 13-typescript-tutorial/src/tutorial.ts
+++ 13-typescript-tutorial/src/tutorial.ts
@@ -1,6 +1,66 @@
 // Testing log
 console.log("typescript tutorial");
 
+// 3. Unions and any type
+// Unions
+let tax: number | string = 10;
+tax = 100;
+tax = "1000";
+
+// Literal values
+let requestStatus: "pending" | "success" | "error" = "pending";
+requestStatus = "success";
+requestStatus = "error";
+// requestStatus = "random"; Not allowed because its possible values are literal
+
+// Type: any (not recommended to use. Only as a last resort)
+let notSure: any = 4;
+notSure = "maybe a string instead";
+notSure = false;
+
+// Books example
+const books = ["1984", "Brave New World", "Fahrenheit 451"];
+let foundBook: string | undefined; // Will be any by default if we're not gonna assign the type
+
+// Loop through the books, look for 1984
+for (let book of books) {
+  if ((book = "1984")) {
+    foundBook = book;
+    break;
+  }
+}
+console.log(foundBook);
+
+// Discount example
+let discount: number | string = 20;
+discount = "20%";
+// discount = true; Not allowed because type is NUMBER or STRING
+
+let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+
+orderStatus = "shipped";
+// orderStatus = "cancelled";  Not allowed because its possible values are literal
+
+/* ---------------------------------------- */
+
+// 1. Basics
+// Declare interface
+interface someValue1 {
+  name: string;
+  id: number;
+}
+
+// Create object with assigned interface
+let someObj1: someValue1 = {
+  name: "random",
+  id: 123,
+};
+
+// Log the object
+console.log(someObj1);
+
+/* ---------------------------------------- */
+
 // 2. Assigning types with annotations
 // String
 let awesomeName2: string = "shakeAndBake";
@@ -20,21 +80,3 @@ let isAwesome2: boolean = true;
 isAwesome2 = false;
 // isAwesome2 = "shakeAndBake"; Not allowed because type is BOOLEAN
 console.log(isAwesome2);
-
-/* ---------------------------------------- */
-
-// 1. Basics
-// Declare interface
-interface someValue1 {
-  name: string;
-  id: number;
-}
-
-// Create object with assigned interface
-let someObj1: someValue1 = {
-  name: "random",
-  id: 123,
-};
-
-// Log the object
-console.log(someObj1);
