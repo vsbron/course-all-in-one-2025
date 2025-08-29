9a4ebf7618ad0c2aa8d039dfed8493afbcb105cd
vsbron <vsbron@gmail.com>
2025-08-29 15:28:13 +0300

    13-typescript-tutorial - Add Basics example

---
+++ 13-typescript-tutorial/src/tutorial.ts
@@ -0,0 +1,18 @@
+// Testing log
+console.log("typescript tutorial");
+
+// 1. Basics
+// Declare interface
+interface someValue {
+  name: string;
+  id: number;
+}
+
+// Create object with assigned interface
+let someObj: someValue = {
+  name: "random",
+  id: 123,
+};
+
+// Log the object
+console.log(someObj);
