1e1e9585da2e60d227a0d133a6408297a0c2ce1c
vsbron <vsbron@gmail.com>
2025-09-01 14:14:37 +0300

    13-typescript-tutorial - Set up adding a new task to the array and render it on the display

--- 13-typescript-tutorial/src/tasks.ts
+++ 13-typescript-tutorial/src/tasks.ts
@@ -20,10 +20,32 @@ taskForm?.addEventListener("submit", (e) => {
   // Get the value from the input field
   const taskDescription = formInput?.value;
 
+  // Handle the form input value
   if (taskDescription) {
-    console.log(taskDescription);
+    // Add the task
+    const task: Task = { description: taskDescription, isCompleted: false };
+    addTask(task);
+
+    // Render tasks
+    renderTask(task);
+    // Update local storage
+
     formInput.value = "";
     return;
   }
   alert("Please enter a task description");
 });
+
+// Function that adds the task to the tasks list
+function addTask(task: Task): void {
+  tasks.push(task);
+}
+
+function renderTask(task: Task): void {
+  // Create LI element and assign the description to it
+  const taskElement = document.createElement("li");
+  taskElement.textContent = task.description;
+
+  // Append the task to the list
+  taskListElement?.appendChild(taskElement);
+}
