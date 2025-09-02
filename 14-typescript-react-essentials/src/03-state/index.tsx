5d8571cb956257c57ca26d6edc595eed9eca7b74
vsbron <vsbron@gmail.com>
2025-09-02 10:34:29 +0300

    14-typescript-react-essentials - 03 - Add a state example with custom type

--- 14-typescript-react-essentials/src/03-state/index.tsx
+++ 14-typescript-react-essentials/src/03-state/index.tsx
@@ -1,18 +1,45 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 
+// Create the type for Link
+type Link = {
+  id: number;
+  url: string;
+  text: string;
+};
+
+// Create the navLinks array
+const navLinks: Link[] = [
+  { id: 1, url: "some url 1", text: "some text 1" },
+  { id: 2, url: "some url 2", text: "some text 2" },
+  { id: 3, url: "some url 3", text: "some text 3" },
+];
+
+// The Component component
 function Component() {
   // Create state values for primitives
   const [text, setText] = useState<string>("shakeAndBake");
   const [number, setNumber] = useState<number>(3);
   const [list, setList] = useState<string[]>([]);
 
+  // Create state value with custom types
+  const [links, setLinks] = useState<Link[]>(navLinks);
+
   // Returned JSX
   return (
     <div>
       <h2 className="mb-1">React & Typescript</h2>
       <h4>{text}</h4>
-      <button className="btn btn-center" onClick={() => setText("Hello")}>
+      <button
+        className="btn btn-center"
+        onClick={() => {
+          setText("Hello");
+          setLinks([
+            ...links,
+            { id: 4, url: "some url 4", text: "some text 4" },
+          ]);
+        }}
+      >
         Click me Text
       </button>
     </div>
