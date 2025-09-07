c64aac21ce94c87d13b6a36deec789118321b2ad
vsbron <vsbron@gmail.com>
2025-09-07 12:29:30 +0300

    15-nextjs-tutorial - Minor improvement for getting all the values automatically

--- 15-nextjs-tutorial/utils/action.ts
+++ 15-nextjs-tutorial/utils/action.ts
@@ -6,6 +6,9 @@ export const createUser = async (formData: FormData) => {
   const firstName = formData.get("firstName") as string;
   const lastName = formData.get("lastName") as string;
 
+  const rawData = Object.fromEntries(formData);
+  console.log(rawData);
+
   // Log the results
-  console.log(firstName, lastName);
+  console.log({ firstName, lastName });
 };
