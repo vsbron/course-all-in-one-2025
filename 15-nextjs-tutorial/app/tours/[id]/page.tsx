7e545dd69a09bc09dd2e8b22a64a70bdf584af31
vsbron <vsbron@gmail.com>
2025-09-07 11:09:29 +0300

    15-nextjs-tutorial - Image tag usage base example

--- 15-nextjs-tutorial/app/tours/[id]/page.tsx
+++ 15-nextjs-tutorial/app/tours/[id]/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+
+import mapsImg from "@/images/maps.jpg";
+
 async function page({ params }: { params: { id: string } }) {
   // Await the params
   const { id } = await params;
@@ -6,6 +10,22 @@ async function page({ params }: { params: { id: string } }) {
   return (
     <div>
       <h1 className="text-4xl">ID: {id}</h1>
+      <section className="flex gap-x-4 mt-4">
+        {/* LOCAL IMAGE */}
+        <div>
+          <Image
+            src={mapsImg}
+            width={192}
+            height={192}
+            priority
+            alt="Maps"
+            className="w-48 h-48 object-cover rounded"
+          />
+          <h2>Local image</h2>
+        </div>
+        {/* REMOTE IMAGE */}
+        <div></div>
+      </section>
     </div>
   );
 }
