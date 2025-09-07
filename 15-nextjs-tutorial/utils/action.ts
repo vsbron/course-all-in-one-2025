cd679757bbafc75403c7a9b6c639d025e4c1fcef
vsbron <vsbron@gmail.com>
2025-09-07 12:47:51 +0300

    15-nextjs-tutorial - Add actions that allows us to read and write the users into a local file

--- 15-nextjs-tutorial/utils/action.ts
+++ 15-nextjs-tutorial/utils/action.ts
@@ -1,14 +1,45 @@
 "use server";
 
+import { readFile, writeFile } from "fs/promises";
+
+type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+};
+
 // Create the action for form
 export const createUser = async (formData: FormData) => {
   // Get the values (with type assertion MUST)
   const firstName = formData.get("firstName") as string;
   const lastName = formData.get("lastName") as string;
 
-  const rawData = Object.fromEntries(formData);
-  console.log(rawData);
+  // // Getting all values automatically
+  // const rawData = Object.fromEntries(formData);
+  // console.log(rawData);
+
+  // Create a new user object
+  const newUser: User = { firstName, lastName, id: Date.now().toString() };
+
+  // Save user to the file
+  saveUser(newUser);
 
   // Log the results
   console.log({ firstName, lastName });
+};
+
+// Create the function to fetch the users list
+export const fetchUsers = async (): Promise<User[]> => {
+  const result = await readFile("users.json", { encoding: "utf8" });
+  const users = result ? JSON.parse(result) : [];
+  return users;
+};
+
+// Helper function to save user
+const saveUser = async (newUser: User) => {
+  // Get the current users list
+  const currentUsers = await fetchUsers();
+
+  // Save all the data to the actual file
+  await writeFile("users.json", JSON.stringify([...currentUsers, newUser]));
 };
