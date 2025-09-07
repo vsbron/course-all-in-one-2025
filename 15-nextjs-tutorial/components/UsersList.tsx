fe069f790de77e28b48317b1dc5549a6735f6b4c
vsbron <vsbron@gmail.com>
2025-09-07 12:54:23 +0300

    15-nextjs-tutorial - Set up the users display from the file in the USersList component

--- 15-nextjs-tutorial/components/UsersList.tsx
+++ 15-nextjs-tutorial/components/UsersList.tsx
@@ -1,6 +1,25 @@
-function UsersList() {
+import { fetchUsers } from "@/utils/action";
+
+async function UsersList() {
+  // Get the users
+  const users = await fetchUsers();
+
   // Returned JSX
-  return <h3 className="text-xl">UsersList</h3>;
+  return (
+    <div className="mt-4">
+      {users.length ? (
+        <div>
+          {users.map(({ id, firstName, lastName }) => (
+            <h4 key={id} className="capitalize text-lg">
+              {firstName} {lastName}
+            </h4>
+          ))}
+        </div>
+      ) : (
+        <p>No users found</p>
+      )}
+    </div>
+  );
 }
 
 export default UsersList;
