7c1269d5a3f519887ba4d2b12cc5af407a587c43
vsbron <vsbron@gmail.com>
2025-09-08 12:11:16 +0300

    15-nextjs-tutorial - Simple POST request route handler

--- 15-nextjs-tutorial/app/api/users/route.ts
+++ 15-nextjs-tutorial/app/api/users/route.ts
@@ -1,6 +1,7 @@
 import { fetchUsers, saveUser } from "@/utils/action";
 import { NextRequest, NextResponse } from "next/server";
 
+// GET request handler
 export const GET = async (req: NextRequest) => {
   // // Get the search params from URL and pull the ID
   // const { searchParams } = new URL(req.url);
@@ -11,9 +12,23 @@ export const GET = async (req: NextRequest) => {
   console.log(req.url);
   console.log(req.nextUrl.searchParams.get("id"));
 
-  // Get the users
+  // Get the users and return them
   const users = await fetchUsers();
+  return Response.json({ users });
 
-  // Return the users as a response
-  return NextResponse.redirect(new URL("/", req.url));
+  // Return redirect
+  // return NextResponse.redirect(new URL("/", req.url));
+};
+
+// POST request handler
+export const POST = async (req: Request) => {
+  // Get the user details and create new object
+  const user = await req.json();
+  const newUser = { ...user, id: Date.now().toString() };
+
+  // Save the user
+  await saveUser(newUser);
+
+  // Return success messaghe
+  return Response.json({ msg: "User created" });
 };
