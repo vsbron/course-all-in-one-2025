6a4bc52067414ad8b89607d8e1cd49154890cda3
vsbron <vsbron@gmail.com>
2025-09-08 11:20:57 +0300

    15-nextjs-tutorial - Enhanced GET example with NextRequest and NextResponse

--- 15-nextjs-tutorial/app/api/users/route.ts
+++ 15-nextjs-tutorial/app/api/users/route.ts
@@ -1,9 +1,19 @@
 import { fetchUsers, saveUser } from "@/utils/action";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (req: NextRequest) => {
+  // // Get the search params from URL and pull the ID
+  // const { searchParams } = new URL(req.url);
+  // const id = searchParams.get("id");
+  // console.log(id);
+
+  // Simpler version with NextRequest
+  console.log(req.url);
+  console.log(req.nextUrl.searchParams.get("id"));
+
   // Get the users
   const users = await fetchUsers();
 
   // Return the users as a response
-  return Response.json(users);
+  return NextResponse.redirect(new URL("/", req.url));
 };
